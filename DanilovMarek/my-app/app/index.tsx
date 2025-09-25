e50212424b978961a718a16ca667344110ca774b
Marek Danilov <danmarekb@gmail.com>
2025-09-25 19:30:22 +0900

    add UseState functionality

--- DanilovMarek/my-app/app/index.tsx
+++ DanilovMarek/my-app/app/index.tsx
@@ -1,15 +1,46 @@
-import { Text, View } from "react-native";
+import { Button } from "@react-navigation/elements";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
-  return (
+	const [str, setStr] = useState('');
+	const [strLen, setStrLen] = useState(str.length)
+	return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      	style={{
+			flex: 1,
+			justifyContent: "center",
+			alignItems: "center",
+      	}}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+		<Text>Use state.</Text>
+		<Text>Динамичная длина строки:</Text>
+		<TextInput
+			placeholder="Введите что-то"
+			onChangeText= {(value) => {
+				setStr(value)
+				setStrLen(value.length)
+			}}
+			style={styles.textInputStyle}
+		>
+			{str}
+		</TextInput>
+		<Button 
+			onPress={() => {
+				setStr('')
+				setStrLen(0)
+			}}
+		>
+			Очистить
+		</Button>
+		<Text>Длина строки: {strLen}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+	textInputStyle: {
+		borderWidth: 1,
+		borderColor: 'black'
+	}
+});
