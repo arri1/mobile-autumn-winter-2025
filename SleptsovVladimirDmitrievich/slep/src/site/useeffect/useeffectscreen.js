195711ecf4f451c91898cd17a229c14c778445da
VERTEXSoftware <vertexsoftwork@gmail.com>
2025-10-02 03:07:54 +0900

    upd 3 UseMemo and UseEffect fix 1

--- SleptsovVladimirDmitrievich/slep/src/site/useeffect/useeffectscreen.js
+++ SleptsovVladimirDmitrievich/slep/src/site/useeffect/useeffectscreen.js
@@ -3,10 +3,8 @@ import {
   View, 
   Text, 
   StyleSheet,
-  StatusBar,
   TouchableOpacity,
-  Modal,
-  Switch
+  Modal
 } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
