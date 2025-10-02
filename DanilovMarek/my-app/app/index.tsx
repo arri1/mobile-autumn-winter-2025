a2679c60bc6de768b21bddf883248e6501adfa1d
Marek Danilov <danmarekb@gmail.com>
2025-10-02 19:54:37 +0900

    add UseEffect functionality

--- DanilovMarek/my-app/app/index.tsx
+++ DanilovMarek/my-app/app/index.tsx
@@ -1,36 +1,39 @@
 import { Button } from "@react-navigation/elements";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
 	const [str, setStr] = useState('');
-	const [strLen, setStrLen] = useState(str.length)
+	const [strLen, setStrLen] = useState(str.length);
+	const [isNameLoad, setNameLoad] = useState(false);
+
+	useEffect(() => {
+		fetch(`https://jsonplaceholder.typicode.com/users/${Math.floor(Math.random() * 10 + 1)}`)
+			.then(response => response.json())
+			.then(user => { setStr('Привет, ' + user.name + '!') })
+			.catch(error => console.log(error))
+			.finally(() => { setNameLoad(false) })
+	}, [isNameLoad])
+
+	useEffect(() => {
+		setStrLen(str.length)
+	}, [str])
+
 	return (
-    <View
-      	style={{
-			flex: 1,
-			justifyContent: "center",
-			alignItems: "center",
-      	}}
-    >
-		<Text>Use state.</Text>
+    <View style={styles.style}>
+		<Text>UseState и UseEffect</Text>
 		<Text>Динамичная длина строки:</Text>
 		<TextInput
 			placeholder="Введите что-то"
-			onChangeText= {(value) => {
-				setStr(value)
-				setStrLen(value.length)
-			}}
+			onChangeText= {(value) => { setStr(value) }}
 			style={styles.textInputStyle}
 		>
 			{str}
 		</TextInput>
-		<Button 
-			onPress={() => {
-				setStr('')
-				setStrLen(0)
-			}}
-		>
+		<Button onPress={() => {setNameLoad(true)}}>
+			Приветствие
+		</Button>
+		<Button onPress={() => { setStr('') }}>
 			Очистить
 		</Button>
 		<Text>Длина строки: {strLen}</Text>
@@ -39,6 +42,13 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+	style: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+    gap: 10
+    },
+
 	textInputStyle: {
 		borderWidth: 1,
 		borderColor: 'black'
