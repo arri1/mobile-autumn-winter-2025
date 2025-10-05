5e72a631462d4e0646652e370fd4fd296335467f
Marek Danilov <danmarekb@gmail.com>
2025-10-05 22:15:48 +0900

    restore old functionality

--- DanilovMarek/my-app/App.tsx
+++ DanilovMarek/my-app/App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const [str, setStr] = useState('');
+        const [strLen, setStrLen] = useState(str.length);
+        const [isNameLoad, setNameLoad] = useState(false);
+    
+        useEffect(() => {
+            fetch(`https://jsonplaceholder.typicode.com/users/${Math.floor(Math.random() * 10 + 1)}`)
+                .then(response => response.json())
+                .then(user => { setStr('Привет, ' + user.name + '!') })
+                .catch(error => console.log(error))
+                .finally(() => { setNameLoad(false) })
+        }, [isNameLoad])
+    
+        useEffect(() => {
+            setStrLen(str.length)
+        }, [str])
+    
+        return (
+        <View style={styles.style}>
+            <Text>UseState и UseEffect</Text>
+            <Text>Динамичная длина строки:</Text>
+            <TextInput
+                placeholder="Введите что-то"
+                onChangeText= {(value) => { setStr(value) }}
+                style={styles.textInputStyle}
+            >
+                {str}
+            </TextInput>
+            <Button title="Приветствие" onPress={() => {setNameLoad(true)}} />
+            <Button title="Очистить" onPress={() => { setStr('') }}/>
+            <Text>Длина строки: {strLen}</Text>
+        </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+        style: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        gap: 10
+        },
+    
+        textInputStyle: {
+            borderWidth: 1,
+            borderColor: 'black'
+        }
+    });
