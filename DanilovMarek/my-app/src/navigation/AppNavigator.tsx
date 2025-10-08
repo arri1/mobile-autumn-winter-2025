5fb7e88280f122bc7b77cef0eead387f33e14042
Marek Danilov <danmarekb@gmail.com>
2025-10-08 22:50:23 +0900

    add useMemo functionality

--- DanilovMarek/my-app/src/navigation/AppNavigator.tsx
+++ DanilovMarek/my-app/src/navigation/AppNavigator.tsx
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import useEffect from "../screens/UseEffect";
 import useState from "../screens/UseState";
+import useMemo from "../screens/UseMemo";
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export default function AppNavigator() {
         <Tab.Navigator>
             <Tab.Screen name="UseState" component={useState} />
             <Tab.Screen name="UseEffect" component={useEffect} />
+            <Tab.Screen name="UseMemo" component={useMemo} />
         </Tab.Navigator>
     );
 }
