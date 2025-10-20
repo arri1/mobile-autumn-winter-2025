c405f506375a8a5c2dc55f75fb7c2dc80a6a3f24
Marek Danilov <danmarekb@gmail.com>
2025-10-20 20:25:41 +0900

    add zustand

--- DanilovMarek/my-app/src/screens/UseEffect.tsx
+++ DanilovMarek/my-app/src/screens/UseEffect.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import Timer from "../components/Timer";
+
 
 export default function UseEffect() {
     const [str, setStr] = useState('');
@@ -14,23 +16,28 @@ export default function UseEffect() {
     }, [isNameLoad])
 
     return (
-    <View style={styles.style}>
-        <Text>UseEffect</Text>
-        <Text>Приветствие:</Text>
-        <Text>{str}</Text>
-        <Button title="Другое имя" onPress={() => {setNameLoad(true)}} />
+    <View style={styles.view2}>
+        <Timer />
+        <View style={styles.view1}>
+            <Text>UseEffect</Text>
+            <Text>Приветствие:</Text>
+            <Text>{str}</Text>
+            <Button title="Другое имя" onPress={() => {setNameLoad(true)}} />
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    style: {
+    view1: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
     gap: 10
     },
-
+    view2: {
+        flex: 1,
+    },
     textInputStyle: {
         borderWidth: 1,
         borderColor: 'black'
--- DanilovMarek/my-app/src/screens/UseState.tsx
+++ DanilovMarek/my-app/src/screens/UseState.tsx
@@ -1,41 +1,46 @@
 import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import Timer from "../components/Timer";
 
 export default function UseState() {
     const [str, setStr] = useState('');
     const [strLen, setStrLen] = useState(str.length);
     return (
-    <View style={styles.style}>
-        <Text>UseState</Text>
-        <Text>Динамичная длина строки:</Text>
-        <TextInput
-            placeholder="Введите что-то"
-            onChangeText= {(value) => { 
-                setStr(value);
-                setStrLen(value.length);
-            }}
-            style={styles.textInputStyle}
-        >
-            {str}
-        </TextInput>
-        <Button title="Очистить" onPress={() => { 
-            setStr('');
-            setStrLen(0)
-
-        }}/>
-        <Text>Длина строки: {strLen}</Text>
+    <View style={styles.view1}>
+        <Timer />
+        <View style={styles.view2}>
+            <Text>UseState</Text>
+            <Text>Динамичная длина строки:</Text>
+            <TextInput
+                placeholder="Введите что-то"
+                onChangeText= {(value) => {
+                    setStr(value);
+                    setStrLen(value.length);
+                }}
+                style={styles.textInputStyle}
+            >
+                {str}
+            </TextInput>
+            <Button title="Очистить" onPress={() => {
+                setStr('');
+                setStrLen(0)
+            }}/>
+            <Text>Длина строки: {strLen}</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    style: {
+    view1: {
+        flex: 1,
+    },
+    view2: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-    gap: 10
+        gap: 10
     },
-
     textInputStyle: {
         borderWidth: 1,
         borderColor: 'black'
