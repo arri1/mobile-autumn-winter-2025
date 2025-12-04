13970491cb85e9abff001b2a97afc9f82638cf69
Wykkz <chikaldintimur@gmail.com>
2025-12-04 21:01:13 +0900

    nav for useMemo

--- ChikaldinTimur/mob/app/(tabs)/_layout.tsx
+++ ChikaldinTimur/mob/app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="useMemoLab"
+        options={{
+          title: 'useMemo',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="chevron.right" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
