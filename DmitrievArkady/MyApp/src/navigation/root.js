ca96dd0c6af970e48ee4abd75bf482bf213b866b
Nienn37 <Darkadiy999@gmail.com>
2025-12-11 13:42:39 +0900

    refactor folders

--- DmitrievArkady/MyApp/src/navigation/root.js
+++ DmitrievArkady/MyApp/src/navigation/root.js
@@ -1,16 +1,14 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import UseStateScreen from '../screens/UseStateLab/UseStateScreen';
-import UseEffectScreen from '../screens/UseEffectLab/UseEffectScreen';
-import UseMemoScreen from '../screens/UseMemoLab/UseMemoScreen';
-import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
+import UseStateScreen from '../screens/Main/UseStateScreen';
+import UseEffectScreen from '../screens/Main/UseEffectScreen';
+import UseMemoScreen from '../screens/Main/UseMemoScreen';
+import ProfileScreen from '../screens/Main/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
  
 export default function RootTabs() {
   return (
-    <Tab.Navigator
-
-    >
+    <Tab.Navigator>
       <Tab.Screen name="useState" component={UseStateScreen} />
       <Tab.Screen name="useEffect" component={UseEffectScreen} />
       <Tab.Screen name="useMemo" component={UseMemoScreen} />
