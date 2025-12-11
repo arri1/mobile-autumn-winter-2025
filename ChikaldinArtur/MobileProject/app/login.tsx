2ea1d32c5637e6074359b82c0f496ecf58499d9d
Artur <112675147+Cwucke@users.noreply.github.com>
2025-12-11 19:11:39 +0900

    style changes

--- ChikaldinArtur/MobileProject/app/login.tsx
+++ ChikaldinArtur/MobileProject/app/login.tsx
@@ -27,9 +27,41 @@ export default function Login() {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput placeholder="Пароль" secureTextEntry value={password} onChangeText={setPassword} />
+    <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+      }}>
+      <TextInput 
+      placeholder="Email" 
+      value={email} 
+      onChangeText={setEmail} 
+      style={{
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#999",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 14,
+        fontSize: 16,
+        marginBottom: 12,
+  }}/>
+      <TextInput 
+      placeholder="Пароль" 
+      secureTextEntry 
+      value={password} 
+      onChangeText={setPassword} 
+      style={{
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#999",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 14,
+        fontSize: 16,
+        marginBottom: 12,
+  }}/>
 
       {error ? <Text style={{ color: "red" }}>{error}</Text> : null}
 
--- ChikaldinArtur/MobileProject/app/register.tsx
+++ ChikaldinArtur/MobileProject/app/register.tsx
@@ -6,9 +6,14 @@ import { router } from "expo-router";
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("")
   const [error, setError] = useState("");
 
   const onRegister = async () => {
+    if (password !== confirmPassword) {
+      setError("Пароли не совпадают");
+      return;
+    }
     try {
       await axios.post("https://cloud.kit-imi.info/api/auth/register", {
         email,
@@ -22,9 +27,56 @@ export default function Register() {
   };
 
   return (
-    <View style={{ padding: 20 }}>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <TextInput placeholder="Пароль" secureTextEntry value={password} onChangeText={setPassword} />
+    <View style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+      }}>
+      <TextInput 
+      placeholder="Email" 
+      value={email} 
+      onChangeText={setEmail}
+      style={{
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#999",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 14,
+        fontSize: 16,
+        marginBottom: 12,
+  }}/>
+      <TextInput 
+      placeholder="Пароль" 
+      secureTextEntry 
+      value={password} 
+      onChangeText={setPassword} 
+      style={{
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#999",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 14,
+        fontSize: 16,
+        marginBottom: 12,
+  }}/>
+      <TextInput 
+      placeholder="Подтвердить пароль" 
+      secureTextEntry 
+      value={confirmPassword} 
+      onChangeText={setConfirmPassword} 
+      style={{
+        width: "100%",
+        borderWidth: 1,
+        borderColor: "#999",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 14,
+        fontSize: 16,
+        marginBottom: 12,
+  }}/>
 
       {error ? <Text style={{ color: "red" }}>{error}</Text> : null}
 
