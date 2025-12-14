5f7e28e5de01b5efcf021124932db1d39b358596
yogurt14r <smurfinho03@gmail.com>
2025-12-15 00:26:46 +0900

    add Tab Navigator with Home, Shop, Cart, Profile screens

---
+++ SemenovBogdan/my-app/src/navigation/AppTabs.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ShopScreen from '../screens/ShopScreen';
+import CartScreen from '../screens/CartScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppTabs({ cart, setCart }) {
+	return (
+		<Tab.Navigator
+			initialRouteName="Home"
+			screenOptions={({ route }) => ({
+				tabBarShowLabel: false,
+				tabBarStyle: {
+					height: 60,
+					backgroundColor: '#fff',
+				},
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
+
+					if (route.name === 'Home') iconName = 'home';
+					else if (route.name === 'Shop') iconName = 'pricetags';
+					else if (route.name === 'Cart') iconName = 'cart';
+					else if (route.name === 'Profile') iconName = 'person';
+
+					return <Ionicons name={iconName} size={24} color={focused ? '#3498db' : '#999'} />;
+				},
+			})}
+		>
+			<Tab.Screen name="Home">
+				{props => <HomeScreen {...props} />}
+			</Tab.Screen>
+			<Tab.Screen name="Shop">
+				{props => <ShopScreen {...props} cart={cart} setCart={setCart} {...props} />}
+			</Tab.Screen>
+			<Tab.Screen name="Cart">
+				{props => <CartScreen {...props} cart={cart} />}
+			</Tab.Screen>
+			<Tab.Screen name="Profile" component={ProfileScreen} />
+		</Tab.Navigator>
+	);
+}
