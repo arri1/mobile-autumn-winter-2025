34f448d30281ab3dc5e834048812bfeca4e93b6d
Artur <112675147+Cwucke@users.noreply.github.com>
2025-12-18 10:05:09 +0900

    useEffect update

--- ChikaldinArtur/MobileProject/app/(tabs)/useEffectLab.tsx
+++ ChikaldinArtur/MobileProject/app/(tabs)/useEffectLab.tsx
@@ -1,20 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 export default function UseEffectLab() {
   const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((prev) => prev + 1);
     }, 1000);
-
+     const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+          throw new Error('Ошибка загрузки');
+        }
+        const data = await response.json();
+        setUsers(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", padding:20}}>
       <Text style={{ fontSize: 24 }}>Таймер: {count} сек.</Text>
+       <FlatList
+      data={users}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <Text style={{ padding: 10 }}>
+          {item.name}
+        </Text>
+      )}
+    />
     </View>
   );
 }
