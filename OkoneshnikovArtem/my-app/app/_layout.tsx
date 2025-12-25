1bf1c61c768435e01edc67d2b892c3cfa108a3c7
Artem Okoneshnikov <113405870+ArtemOk-F@users.noreply.github.com>
2025-12-25 18:10:03 +0900

    add auto login on app start

--- OkoneshnikovArtem/my-app/app/_layout.tsx
+++ OkoneshnikovArtem/my-app/app/_layout.tsx
@@ -1,9 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import useAuthStore from '@/store/authStore';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +13,12 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const { loadUser } = useAuthStore();
+
+  // Загрузка пользователя при старте приложения
+  useEffect(() => {
+    loadUser();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
