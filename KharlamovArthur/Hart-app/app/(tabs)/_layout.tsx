de12fad28d26dbc04a04813655226853c12a0a23
KharlArt <harlartur42@gmail.com>
2025-12-25 22:06:51 +0900

    add UseEffect

--- KharlamovArthur/Hart-app/app/(tabs)/_layout.tsx
+++ KharlamovArthur/Hart-app/app/(tabs)/_layout.tsx
@@ -26,10 +26,24 @@ export default function TabLayout() {
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
       }}>
+         <Tabs.Screen
+        name="index"
+        options={{
+          title: 'modal',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
       <Tabs.Screen
         name="UseState"
         options={{
           title: 'Use State',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="UseEffect"
+        options={{
+          title: 'Use Effect',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
